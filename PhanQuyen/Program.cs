ff639c2a9c552ff21678657a833eec5c0ad5cbfc
phucjeya <phucj@DESKTOP-35QKRNL>
2021-04-03 16:21:22 +0700

    ok

--- PhanQuyen/Program.cs
+++ PhanQuyen/Program.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace PhanQuyen
 {
     static class Program
-    {
+    {//ff
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +16,7 @@ namespace PhanQuyen
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            //Application.Run(new Login());
         }
     }
 }
